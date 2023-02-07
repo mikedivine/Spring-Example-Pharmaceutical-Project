8124d98c5070f14163eb59cfc49517861a7b9c9f
MikeDivineCSUMB <116385403+MikeDivineCSUMB@users.noreply.github.com>
2023-02-06 23:49:45 -0800

    committed - 2/6/23 11:49pm

--- src/main/java/com/csumb/cst363/DataGenerate.java
+++ src/main/java/com/csumb/cst363/DataGenerate.java
@@ -1,39 +1,24 @@
 package com.csumb.cst363;
 
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Types;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 /*
   A program to generate 100 random patients,
   10 random doctors
   and 100 random prescriptions.
  */
-@Controller
-public class DataGenerate {
 
-  @Autowired
-  private JdbcTemplate jdbcTemplate;
+public class DataGenerate {
 
   Patient patient;
   Doctor doctor;
@@ -46,6 +31,10 @@ public class DataGenerate {
   static String[] streets;
 
   ArrayList<String> doctorSSNList = new ArrayList<>();
+  ArrayList<String> patientSSNList = new ArrayList<>();
+  static final String DBURL = "jdbc:mysql://localhost:3306/drugco";  // database URL
+  static final String USERID = "root";
+  static final String PASSWORD = "csuMB@)@#";
   public static void main(String[] args) {
     initializeArrays();
     new DataGenerate();
@@ -53,24 +42,120 @@ public class DataGenerate {
 
   public DataGenerate() {
 
-    System.out.println(SSNGenerator());
-    System.out.println(SSNGenerator());
-    System.out.println(SSNGenerator());
-    System.out.println(getRandomDate());
+    for (int i = 0; i < 10; i++) {
+      addDoctor();
+    }
+
+    for (int i = 0; i < 100; i++) {
+      addPatient();
+    }
+
+    for (int i = 0; i < 100; i++) {
+      addPrescription();
+    }
   }
-  public Patient getPatient() {
+
+  public void addPrescription() {
+    Prescription prescription = getPrescription();
+
+    try (Connection con = DriverManager.getConnection(DBURL, USERID, PASSWORD);) {
+      PreparedStatement ps = con.prepareStatement("INSERT INTO prescriptions(patientSSN, doctorSSN, drugID, datePrescribed, Qty) values(?, ?, ?, ?, ?)");
+      ps.setString(1, prescription.getPatientSSN());
+      ps.setString(2, prescription.getDoctorSSN());
+      ps.setInt(3, prescription.getDrugID());
+      ps.setString(4, prescription.getDatePrescribed());
+      ps.setInt(5, prescription.getQty());
+
+      ps.executeUpdate();
+
+    } catch (SQLException e) {
+      System.out.println("MySQL error on prescription add. " + e);
+    }
+  }
+  public void addDoctor() {
+
+    Doctor doctor = getDoctor();
+
+    try (Connection con = DriverManager.getConnection(DBURL, USERID, PASSWORD);) {
+      PreparedStatement ps = con.prepareStatement("INSERT INTO doctor(doctorSSN, fName, lName, specialty, startDate) values(?, ?, ?, ?, ?)");
+      ps.setString(1, doctor.getDoctorSSN());
+      ps.setString(2, doctor.getfName());
+      ps.setString(3, doctor.getlName());
+      ps.setString(4, doctor.getSpecialty());
+      ps.setString(5, doctor.getStartDate());
+
+      ps.executeUpdate();
+
+    } catch (SQLException e) {
+      System.out.println("MySQL error on doctor add. " + e);
+    }
+  }
+
+  public void addPatient() {
+
+      Patient patient = getPatient();
+
+      try (Connection con = DriverManager.getConnection(DBURL, USERID, PASSWORD);) {
+        PreparedStatement ps = con.prepareStatement("INSERT INTO patient(patientSSN, fName, lName, birthdate, street, city, state, zip, doctorSSN) values(?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, patient.getPatientSSN());
+        ps.setString(2, patient.getfName());
+        ps.setString(3, patient.getlName());
+        ps.setString(4, patient.getBirthdate());
+        ps.setString(5, patient.getStreet());
+        ps.setString(6, patient.getCity());
+        ps.setString(7, patient.getState());
+        ps.setString(8, patient.getZip());
+        ps.setString(9, patient.getDoctorSSN());
+
+        ps.executeUpdate();
+
+      } catch (SQLException e) {
+        System.out.println("MySQL error on patient add. " + e);
+      }
+  }
+
+  public Prescription getPrescription() {
+    Prescription prescription = new Prescription();
     Random r = new Random();
-    Integer randInt = r.nextInt(11-1) + 1;
+    Integer randInt = r.nextInt(100-1) + 1;
+    prescription.setPatientSSN(patientSSNList.get(randInt));
+    randInt = r.nextInt(10-1) + 1;
+    prescription.setDoctorSSN(doctorSSNList.get(randInt));
+    randInt = r.nextInt(21-1) + 1;
+    prescription.setDrugID(randInt);
+    prescription.setDatePrescribed(getRandomDate());
+    randInt = r.nextInt(151-1) + 1;
+    prescription.setQty(randInt);
+    return prescription;
+  }
+
+  public Patient getPatient() {
+    Patient patient = new Patient();
+    Random r = new Random();
+    Integer randInt = r.nextInt(10-1) + 1;
     patient.setDoctorSSN(doctorSSNList.get(randInt));
-    patient.setPatientSSN(SSNGenerator());
+    String patientSSN = SSNGenerator();
+    patientSSNList.add(patientSSN);
+    patient.setPatientSSN(patientSSN);
     patient.setfName(getFName());
     patient.setlName(getLName());
+    patient.setBirthdate(getRandomDate());
+    patient.setStreet(getStreet());
+    patient.setCity(getCity());
+    patient.setState(getState());
+    patient.setZip(zipGenerate());
     return patient;
   }
   public Doctor getDoctor() {
+    Doctor doctor = new Doctor();
     String doctorSSN = SSNGenerator();
     doctorSSNList.add(doctorSSN);
     doctor.setDoctorSSN(doctorSSN);
+    doctor.setfName(getFName());
+    doctor.setlName(getLName());
+    doctor.setSpecialty(getSpecialty());
+    doctor.setStartDate(getRandomDate());
     return doctor;
   }
 
@@ -127,11 +212,24 @@ public class DataGenerate {
     Integer randInt = r.nextInt(4096-1) + 1;
     return listOfFNames[randInt];
   }
+
+  public String getState() {
+    Random r = new Random();
+    Integer randInt = r.nextInt(51-1) + 1;
+    return states[randInt];
+  }
+
+  public String getSpecialty() {
+    Random r = new Random();
+    Integer randInt = r.nextInt(39-1) + 1;
+    return specialties[randInt];
+  }
+
   public String SSNGenerator() {
     String SSN;
-    String  SSNstart;
-    String  SSNmiddle;
-    String  SSNend;
+    String SSNstart;
+    String SSNmiddle;
+    String SSNend;
     Random r = new Random();
 
     Integer start = r.nextInt(900-1) + 1;
@@ -164,7 +262,7 @@ public class DataGenerate {
       SSNend = end.toString();
     }
 
-    return SSNstart + "-" + SSNmiddle + "-" + SSNend;
+    return SSNstart + SSNmiddle + SSNend;
   }
 
   public String zipGenerate() {
@@ -173,9 +271,17 @@ public class DataGenerate {
     Integer zipcode = r.nextInt(99999-601) + 601;
     return zipcode.toString();
   }
-  private Connection getConnection() throws SQLException {
-    Connection conn = jdbcTemplate.getDataSource().getConnection();
-    return conn;
+
+  public String getStreet() {
+    Random r = new Random();
+    Integer randInt = r.nextInt(153-1) + 1;
+    return streets[randInt];
+  }
+
+  public String getCity() {
+    Random r = new Random();
+    Integer randInt = r.nextInt(1961-1) + 1;
+    return cities[randInt];
   }
 
   public static String[] getArray(String file)
